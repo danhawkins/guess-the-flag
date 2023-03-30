d249c2f75faac6101268b1c59552e620263c0aaa
Danny Hawkins <danny@quiqup.com>
2023-03-30 21:26:36 +0400

    Current

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+struct Country {
+  var name: String
+  var code: String
+}
+
 struct TitleModifier : ViewModifier {
   func body(content: Content) -> some View {
           content
@@ -21,19 +26,21 @@ extension View {
 }
 
 struct FlagImage: View {
-  var country: String
+  var country: Country
   
   var body: some View {
-    Image(country)
-      .renderingMode(.original)
+    Image(country.code.lowercased())
+      .resizable()
+      .aspectRatio( contentMode: .fit)
       .shadow(radius: 4)
+      .frame(width: 400, height: 150)
       .clipShape(RoundedRectangle(cornerRadius: 5))
   }
 }
 
 struct ContentView: View {
-  @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-  @State var correctAnswer = Int.random(in: 0...2)
+  @State var countries: [Country] = []
+  @State var correctAnswer = -1
   
   @State private var showingScore = false
   @State private var scoreTitle = ""
@@ -43,36 +50,44 @@ struct ContentView: View {
   var body: some View {
     ZStack {
       RadialGradient(stops: [
-          .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-          .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+        .init(color: Color(red: 0.76, green: 0.1, blue: 0.1), location: 0.2),
+        .init(color: Color(red: 0.2, green: 0.2, blue: 0.2), location: 0.2),
       ], center: .top, startRadius: 200, endRadius: 400)
-          .ignoresSafeArea()
-      VStack{
-        Spacer()
-        Text("Guess the Flag").titleText()
-         
-          .foregroundColor(.white)
-        Spacer()
-        Spacer()
-        Text("Score: ???")
+      .ignoresSafeArea()
+      if correctAnswer > 0 {
+        VStack{
+          Text("Guess the Flag").titleText()
+            .foregroundColor(.white)
+            .padding(.bottom)
+          Text("Score: ???")
             .foregroundColor(.white)
             .font(.title.bold())
-        Spacer()
-        VStack(spacing: 15) {
-          VStack {
-            Text("Tap the flag of").foregroundColor(.secondary).font(.subheadline.weight(.heavy))
-            Text(countries[correctAnswer]).foregroundColor(.white).font(.largeTitle.weight(.semibold))
-          }
-          ForEach(0 ..< 3) { number in
-            Button {
-              flagTapped(number)
-            } label: {
-              FlagImage(country: countries[number])
+          Spacer()
+          VStack(spacing: 15) {
+            VStack {
+              Text("Tap the flag of").foregroundColor(.secondary).font(.subheadline.weight(.heavy))
+              Text(countries[correctAnswer].name).foregroundColor(.white).font(.largeTitle.weight(.semibold))
+            }
+            ForEach(0 ..< 3) { number in
+              Button {
+                flagTapped(number)
+              } label: {
+                FlagImage(country: countries[number])
+
+              }
             }
           }
+          Spacer()
+        }.padding()
+      } else {
+        VStack{
+          Text("Loading")
         }
-      }.padding()
-    }.alert(scoreTitle, isPresented: $showingScore) {
+      }
+      
+    }
+    .onAppear(perform: load)
+    .alert(scoreTitle, isPresented: $showingScore) {
       Button("Continue", action: askQuestion)
     } message: {
       Text("Your score is ???")
@@ -91,7 +106,19 @@ struct ContentView: View {
   
   func askQuestion() {
     countries.shuffle()
-    correctAnswer = Int.random(in: 0...2)
+    correctAnswer = Int.random(in: 0...3)
+  }
+  
+  func load() {
+    guard let url = Bundle.main.url(forResource: "countries", withExtension: "csv") else { return }
+    guard let contents = try? String(contentsOf: url) else { return }
+    for row in contents.components(separatedBy: .newlines) {
+      let data = row.components(separatedBy: ",")
+      if data.count == 2 {
+        countries.append(Country(name: data[1], code: data[0]))
+      }
+    }
+    correctAnswer = Int.random(in: 0...3)
   }
 }
 
